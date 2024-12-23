51f0c562b6e0ecbe154c41a589452357b648d71e
unknown <guillerm.aldana@gmail.com>
2024-12-23 00:45:32 -0300

    feat: implement routes and provider mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
+
+import RoutesConfiguration from './routers';
+import Navbar from './components/Navbar';
+
 import '@fontsource/ubuntu';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="content">
-      <Typography variant='h3'>Rsbuild with React</Typography>
-      <Typography variant='h6'>Start building amazing things with Rsbuild.</Typography>
-    </div>
+    <>
+      <Navbar />
+      <Box sx={{ backgroundColor: '#ffffffe6', minHeight: '100vh' }}>
+        <RoutesConfiguration />
+      </Box>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './Theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
     </React.StrictMode>,
   );
 }
